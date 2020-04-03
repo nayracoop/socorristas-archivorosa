e53b3c3c2b6e3d2e113b693dfe4419bcfa63bfde
Ignacio Guerra <ignacio.guerra@nayra.coop>
2020-04-03 12:29:55 -0300

    Router added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
-import ChapterHeader from './components/layout/templates/ChapterHeader'
-import IndexGrid from './components/layout/templates/IndexGrid'
+import { Route, withRouter } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
+import ChapterHeader from './components/layout/templates/ChapterHeader';
+import IndexGrid from './components/layout/templates/IndexGrid';
 
-const background = require('./assets/imgs/img01.jpg')
+const background = require('./assets/imgs/img01.jpg');
+const theme = { };
 
 class App extends Component {
 
   render() {
     return (
       <div className="app">
-        <IndexGrid></IndexGrid>
-        <ChapterHeader backgroundImage={background}></ChapterHeader>
+        <ThemeProvider theme={theme}>
+          <Route path="/" exact>
+            <IndexGrid></IndexGrid>
+            <ChapterHeader backgroundImage={background}></ChapterHeader>
+          </Route>
+          <Route path="/post">
+            Hola :D
+          </Route>
+        </ThemeProvider>
       </div>
       );
   }
 }
 
-export default App;
+export default withRouter(App);
