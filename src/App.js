e2957d47805c2d9e445221b65354c38ef0d77ca9
Romina Gómez <romina.gomez@nayra.coop>
2020-04-03 12:25:06 -0300

    text headers and layout structure update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ChapterHeader from './components/layout/templates/ChapterHeader'
 import IndexGrid from './components/layout/templates/IndexGrid'
 
-const background = require('./assets/imgs/img01.jpg')
+const background = require('./assets/imgs/background.png')
 
 class App extends Component {
 
