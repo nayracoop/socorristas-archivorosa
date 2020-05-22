36d2891981768c90a25af1c773713ae3249a5728
Ignacio Guerra <ignacio.guerra@nayra.coop>
2020-05-22 15:58:00 -0300

    New static images

--- src/components/layout/snippets/header/ChapterHeaderImage.js
+++ src/components/layout/snippets/header/ChapterHeaderImage.js
@@ -18,6 +18,12 @@ const Image = styled.img`
 const ChapterHeaderImage = (props) => {
     return (
         <Switch>
+            <Route path="/la-buena-noticia">
+                <Image src={props.image} />
+            </Route>
+            <Route path="/viento-en-venecia" exact>
+                <BassinetBranch />
+            </Route>
             <Route path="/hay-abortos" exact>
                 <ButterflyBike />
             </Route>
@@ -27,14 +33,23 @@ const ChapterHeaderImage = (props) => {
             <Route path="/lina" exact>
                 <CloudsBench />
             </Route>
-            <Route path="/el-palpitar-de-los-abortos" exact>
-                <HeartChat />
+            <Route path="/quiero-que-salga">
+                <Image src={props.image} />
             </Route>
-            <Route path="/viento-en-venecia" exact>
-                <BassinetBranch />
+            <Route path="/nilda">
+                <Image src={props.image} />
             </Route>
             <Route path="/del-socorro-del-socorro" exact>
                 <BirdChair />
+            </Route>
+            <Route path="/urgencia-rosa" exact>
+                <BirdChair />
+            </Route>
+            <Route path="/cuando-estamos-juntas">
+                <Image src={props.image} />
+            </Route>
+            <Route path="/el-palpitar-de-los-abortos" exact>
+                <HeartChat />
             </Route>
             <Route path="/">
                 <Image src={props.image} />
